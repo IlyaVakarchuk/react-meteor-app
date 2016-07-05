078ce0f919c59d311afb88e0ce8876b709760bfa
Ilya Vakarchuk <ilya.vakarchuk@noveogroup.com>
2016-07-05 10:25:08 +0600

    route and page for 404 error

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -10,7 +10,7 @@ import About from './About';
 import Home from './Home';
 import Navigation from './Navigation';
 import SplashScreen from './SplashScreen';
-
+import NotFoundPage from './NotFoundPage';
 
 let Page = class Page extends React.Component {
   constructor() {
@@ -52,12 +52,12 @@ const App = class App extends React.Component {
           <Route path="auth" component={Auth}/>
           <Route path="about" component={About} onEnter={this.onCheckAuth} />
           <Route path="home" component={Home} onEnter={this.onCheckAuth} />
+          <Route path="*" component={NotFoundPage} />
         </Route>
       </Router>
       </div>
     );  
   }
 }
-
 
 export default App;
---
+++ imports/ui/NotFoundPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFoundPage = class NotFoundPage extends React.Component {
+  render () {
+    return (
+      <div id='not-found-page'>
+        <img src="images/interface/404.png" />
+        <div>404 Not Found</div>
+      </div>
+    )
+  }
+};
+
+export default NotFoundPage; 
