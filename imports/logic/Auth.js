68917f4b07e4801dcd0cf8561c43a9d92ba5219a
Ilya Vakarchuk <ilyavakarchuk@gmail.com>
2016-07-05 23:01:20 +0600

    add validate data before registrate user

--- imports/logic/Auth.js
+++ imports/logic/Auth.js
@@ -5,6 +5,11 @@ import { Session } from 'meteor/session';
 import { browserHistory } from 'react-router'
 
 const Auth = class {
+
+	constructor() {
+		this.emailPattern = /[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}/igm;
+	}
+
 	auth(params) {	
 		Meteor.loginWithPassword(params.login, params.password, (err) => {
     	if (!err) {
@@ -15,12 +20,20 @@ const Auth = class {
 	}
 
 	registration(params) {
-		Accounts.createUser({email : params.login, password : params.password}, (err) => {
-			if (!err) {
-    		Session.set('auth', true);
-    		browserHistory.push('home');
-    	}
-		});
+		if ((params.login).match(this.emailPattern) != null) {
+			if (params.password01 === params.password02 ) {
+				Accounts.createUser({email : params.login, password : params.password01}, (err) => {
+					if (!err) {
+		    		Session.set('auth', true);
+		    		browserHistory.push('home');
+		    	}
+				}); 
+			} else {
+				// invalide password
+			}
+		} else {
+			// invalide email
+		}
 	}
 }
 
