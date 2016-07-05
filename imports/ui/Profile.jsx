8804981838b2d9a7adcf125320080e0abd9d6d82
Ilya Vakarchuk <ilya.vakarchuk@noveogroup.com>
2016-07-05 14:11:06 +0600

    markup and style for profile settings block; action for UI elemets

--- imports/ui/Profile.jsx
+++ imports/ui/Profile.jsx
@@ -1,12 +1,47 @@
 import React from 'react';
 
+import Input from './Input';
+import Button from './Button';
+
 const Profile = class Profile extends React.Component {
-  constructor() {
-    super();
+  constructor(props, context) {
+    super(props, context);
 
     this.state = {
-      show : false,
+      username : {
+        state : false,
+        value : ''
+      },
+      email : {
+        state : false,
+        value : ''
+      },
+      password : {
+        state : false,
+        value : ''
+      }
     }
+    
+    this.onEditParam = this.onEditParam.bind(this);
+    this.onSaveParam = this.onSaveParam.bind(this);
+  }
+
+  onEditParam (input) {
+    switch (input) {
+      case 'username' :
+        this.setState({ username : { state : !this.state.username.state}});
+        break;
+      case 'email' :
+        this.setState({email : { state : !this.state.email.state}});
+        break;
+      case 'password' :
+        this.setState({password : { state : !this.state.password.state}});
+        break;
+    }
+  }
+
+  onSaveParam () {
+    
   }
 
   componentWillReceiveProps(nextProps) {
@@ -19,15 +54,27 @@ const Profile = class Profile extends React.Component {
         <div className='title'> Profile Settings </div>
         <div className='username'>
           <span className='section-title'>Username</span>
-          <span className='section-value'>Ilya Vakarchuk<i className="fa fa-pencil" aria-hidden="true"></i></span>
+          <span className='section-value'> 
+            {this.state.username.state ? <Input inputValue={this.state.username} type={ 'text' } placeholder={ 'Username' }/> : <span className='value' onClick={ () => this.onEditParam('username') } >Ilya Vakarchuk</span> } 
+            <i className="fa fa-pencil" aria-hidden="true" onClick={ () => this.onEditParam('username') }></i>
+          </span>
         </div>
         <div className='email'>
           <span className='section-title'>email</span>
-          <span className='section-value'>IlyaVakarchuk@email.com<i className="fa fa-pencil" aria-hidden="true"></i></span>
+          <span className='section-value'> 
+            {this.state.email.state ? <Input inputValue={this.state.email} type={ 'email' } placeholder={ 'Email' }/> : <span className='value' onClick={ () => this.onEditParam('email') } >IlyaVakarchuk@mail.com</span> } 
+            <i className="fa fa-pencil" aria-hidden="true" onClick={ () => this.onEditParam('email') }></i>
+          </span>
         </div>
         <div className='password'>
           <span className='section-title'>password</span>
-          <span className='section-value'>IlyaVakarchuk@email.com<i className="fa fa-pencil" aria-hidden="true"></i></span>
+          <span className='section-value'> 
+            {this.state.password.state ? <Input inputValue={this.state.password} type={ 'password' } placeholder={ 'Password' }/> : <span className='value' onClick={ () => this.onEditParam('password') } >fdsafdsafdsa</span> } 
+            <i className="fa fa-pencil" aria-hidden="true" onClick={ () => this.onEditParam('password') }></i>
+          </span>
+        </div>
+        <div className='save-setting-btn'>
+          { (this.state.username.state || this.state.email.state || this.state.password.state) ? <Button text={'Save'} action={ this.onSaveParam }/> : false }
         </div>
       </div>
     )
