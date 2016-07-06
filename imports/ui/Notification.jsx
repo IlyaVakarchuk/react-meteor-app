b79cb62b4656b408e78ed359d31636c8ee821c3e
Ilya Vakarchuk <ilya.vakarchuk@noveogroup.com>
2016-07-06 14:20:04 +0600

    notification view

---
+++ imports/ui/Notification.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const Notification = class Notification extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      show : false,
+      message : '',
+      delay : 2000
+    }
+    this.onShowNotification = this.onShowNotification.bind(this);
+    this.onHideNotification = this.onHideNotification.bind(this);
+
+  }
+
+  test() {
+    console.log('dfsdfsd')
+  }
+
+  onShowNotification () {
+    this.setState({show : true});
+    setTimeout(this.onHideNotification,this.state.delay);
+  }
+
+  onHideNotification () {
+    this.setState({show : false}); 
+  }
+
+  componentWillReceiveProps(test) {
+    console.log(test)
+    console.log('update')
+  }
+
+  render () {
+    let view = '';
+    console.log('change')
+    if (this.state.show) {
+      view = <div id='notification'> dsv v</div>;
+    } else {
+      view = false;
+    }
+    return (
+      view 
+    )
+  }
+};
+
+export default Notification; 
