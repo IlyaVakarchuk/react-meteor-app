5a935e8223db1c6bcc9f7fbb3ec1d2c5f14847b6
Ilya Vakarchuk <ilyavakarchuk@gmail.com>
2016-07-07 20:41:51 +0600

    markup and style changes

--- imports/ui/SplashScreen.jsx
+++ imports/ui/SplashScreen.jsx
@@ -5,6 +5,10 @@ const SplashScreen = React.createClass({
     return (
      <div id="splash-screen">
       <div className='header'>
+      <div className='title'>
+        <div className='level-01'>RectJS + MeteorJS</div>
+        <div className='level-02'>Test App</div>
+      </div>
         <img src="images/interface/icon.svg" />
       </div>
       <div className='footer'></div> 
